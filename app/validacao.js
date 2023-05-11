6ca2981e70f9cc2254495371799061952bf16f0b
eduvinagre <eduvinagre@gmail.com>
2023-05-11 16:50:28 -0300

    validacao update

--- app/validacao.js
+++ app/validacao.js
@@ -3,10 +3,12 @@ function validaChute(chute) {
 
   if (chuteInvalido(numero)) {
     elementoChute.innerHTML += `<div>Valor inválido</div>`;
+    return;
   }
 
   if (valorMaiorMenor(numero)) {
     elementoChute.innerHTML += `<div>Valor inválido: diga um número entre ${menorValor} e ${maiorValor}</div>`;
+    return;
   }
 
   if (numero === numeroSecreto) {
@@ -14,6 +16,10 @@ function validaChute(chute) {
     <h2>Você acertou!</h2>
     <h3>O número da sorte era ${numeroSecreto}
     `;
+  } else if (numero > numeroSecreto) {
+    elementoChute.innerHTML += `<div>
+        O número secreto é maior <i class="fa-solid fa-arrow-up-long"></i>
+      </div>`;
   }
 }
 
